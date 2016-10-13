4f680d3dd67d3fcad6a98303a462a9f6e2b26e9b
nguyenbichngoc <ngocnguen@gmail.com>
2016-10-13 16:07:59 +0700

    first commit

---
+++ app/src/main/java/com/example/gmo/ngoc/ngocActivity.java
@@ -0,0 +1,13 @@
+package com.example.gmo.ngoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ngocActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngoc);
+    }
+}
